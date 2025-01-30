d8007b8ecead439ffc20e4d6909a853c49eb8786
Stepan Husa <stepan.husa@gmail.com>
2025-01-30 10:19:37 +0100

    WIP continuation.

--- MuCont/run_debug.jl
+++ MuCont/run_debug.jl
@@ -2,13 +2,21 @@ import MuCont as cont
 
 
 
-# run_first
-sys = cont.Models.ODESystem("Test", ["x","y"], ["a","b"], ["x'=a*x+17","y' =a*a+6"])
-c = cont.simple_computer_add(1,2)
-println(c)
-
 
 
 include("run_first.jl")
 import .run_first
-run_first.main()
+# run_first.main()
+
+initials = [-8.0, -7.0]
+params = [-2, 0]
+
+tspan = [0.0, 10.0]
+
+t, y = integrate_euler(fun_eval, initials, params, tspan)
+plot(t, y[:,1], title="Time Series", xlabel="Time", ylabel="Values", label=["x" "y"])
+
+
+equi = y[end,:]
+
+continuate(fun_eval,fun_grad, equi, params, npoints)
--- MuCont/run_first.jl
+++ MuCont/run_first.jl
@@ -1,7 +1,6 @@
-module run_first
 using Plots
 
-import cont
+import MuCont as cont
 
 function fun_eval(t, kmrgd, par_a)
     x = kmrgd[1]
@@ -10,52 +9,103 @@ function fun_eval(t, kmrgd, par_a)
     a = par_a[1]
     b = par_a[2]
 
-    xdot = y
-    ydot = a+b*x+x^2+x*y
+    ## possibly hopf
+    # xdot = y
+    # ydot = a+b*x+x^2+x*y
+
+    
+    #just some linear system
+    # xdot = y
+    # ydot = -x
+
+        #fold
+    xdot = x^2 + a
+    ydot = -y
 
     return [xdot, ydot]
 end
 
-function main()
-    initials = [1.0, 2.0]
-    params = [1.0, -3.0]
+function fun_jacobi(kmrgd,par_a)
+    x = kmrgd[1]
+    y= kmrgd[2]
 
-    tspan = [0.0, 10.0]
+    a = par_a[1]
+    b = par_a[2]
+
+    J = zeros(2, 2)
+    J[1, 1] = 2*x
+    J[1, 2] = 0
+    J[2, 1] = 0
+    J[2, 2] = -1
+
+    return J
+
     
-    timeser = integrate(fun_eval, initials, params, tspan)
-    
-
-
-    plot(timeser, title="Time Series", xlabel="Time", ylabel="Values", label=["x" "y"])
-
 end
 
-function integrate(eval_fun, initials, params, tspan)
+
+function integrate_rk(fun, initials, params, tspan)
     h = 0.1  # Step size
     t0, tf = tspan
     t = t0
     y = initials
-    results = Float64[]
-    times = Float64[]
+    results = []
+    times = []
 
     while t <= tf
-        push!(results, y)
+        push!(results, y')
         push!(times, t)
 
         # Compute Runge-Kutta increments
-        k1 = eval_fun(t, y, params)
-        k2 = eval_fun(t + h/2, y .+ (h/2) .* k1, params)
-        k3 = eval_fun(t + h/2, y .+ (h/2) .* k2, params)
-        k4 = eval_fun(t + h, y .+ h .* k3, params)
+        k1 = fun_eval(t, y, params)
+        k2 = fun_eval(t + h/2, y .+ (h/2) .* k1, params)
+        k3 = fun_eval(t + h/2, y .+ (h/2) .* k2, params)
+        k4 = fun_eval(t + h, y .+ h .* k3, params)
 
         # Update solution
         y = y .+ (h/6) .* (k1 .+ 2 .* k2 .+ 2 .* k3 .+ k4)
         t += h
     end
 
-    return hcat(times, hcat(results...)')  # Combine time and values into a matrix
+    results_matrix = vcat(results...)  # Convert results to a matrix
+    return times, results_matrix  # Return times and matrix of values
+end
+
+function integrate_euler(fun, initials, params, tspan)
+    h = 0.1  # Step size
+    t0, tf = tspan
+    t = t0
+    y = initials
+    results = []
+    times = []
+
+    while t <= tf
+        push!(results, y')
+        push!(times, t)
+
+        # Compute Euler increment
+        k1 = fun_eval(t, y, params)
+
+        # Update solution
+        y = y .+ h .* k1
+        t += h
+    end
+
+    results_matrix = vcat(results...)  # Convert results to a matrix
+    return times, results_matrix  # Return times and matrix of values
 end
 
 
-end
+function continuate(fun_eval, fun_jacobi, initials, params, npoints)
+    x0 = initials
 
+    stepsize = 0.01
+
+    J = fun_jacobi(x0, params)
+    f = fun_eval(0, x0, params)
+
+
+    
+
+
+end
